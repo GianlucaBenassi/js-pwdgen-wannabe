cb565eef442ab9572ccbd8dc3a22d7391901f691
Gianluca Benassi <benox1199@gmail.com>
2021-11-04 14:23:25 +0100

    html print password

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const mySurname = prompt('Inserisci il tuo cognome');
 const myColor = prompt('Inserisci il tuo colore preferito');
 let password;
 
-console.log(myName);
-console.log(mySurname);
-console.log(myColor);
+// console.log(myName);
+// console.log(mySurname);
+// console.log(myColor);
 
 password = myName + mySurname + myColor + '21'
 
-console.log(password);
+// console.log(password);
 
-document.writeln(password);
+document.getElementById('password').innerHTML = password
